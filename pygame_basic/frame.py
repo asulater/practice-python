bb7b2919bad99eff3656f8a23fba701806c4dd17
asulater <asulater@gmail.com>
2021-05-18 00:33:39 +0900

    updates

--- pygame_basic/frame.py
+++ pygame_basic/frame.py
@@ -1,3 +1,9 @@
 import pygame
 
-pygame.init()
+pygame.init() # Initialization
+
+screen_width = 480 
+screen_height = 640 
+screent = pygame.display.set_mode((screen_width, screen_height))
+
+# set display title 
